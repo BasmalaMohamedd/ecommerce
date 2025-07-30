5daad4c0277f9b7bd1fa3b7d9ba4acc7f14635f8
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-07-31 02:14:41 +0300

    backend init functions headers

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,78 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+//===============================Users=============================
+const loginUser = ()=>{
+
+}
+
+const getAllUsers = ()=>{
+
+}
+
+const updateUser = ()=>{
+
+}
+
+const deleteUser = ()=>{
+
+}
+
+const registerUser = ()=>{
+
+}
+
+
+//===============================Products=============================
+
+const getAllProducts = ()=>{
+
+}
+
+const getProductByTitle = ()=>{
+    
+}
+
+const getProductById = ()=>{
+
+}
+
+const deleteProduct = ()=>{
+
+}
+
+const updateProduct = ()=>{
+
+}
+
+const decrementQuatityBy = ()=>{
+
+}
+
+const incrementQuatityBy = ()=>{
+
+}
+
+//===============================Carts=============================
+const addCart = ()=>{
+
+}
+
+const getCart = ()=>{
+
+}
+
+const addProductToCart = ()=>{
+
+}
+
+const emptyCart = ()=>{
+
+}
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,3 +81,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
